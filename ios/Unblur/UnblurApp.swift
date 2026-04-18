9744da2fe5e4777fbb5a8e15d8583de1b40af326
Nathan <zhaody.mu@gmail.com>
2026-04-18 16:31:20 +1000

    feat(T012): wire EpisodeListView as app root, remove SwiftData template
    - Replace SwiftData ModelContainer setup with plain WindowGroup
    - Set EpisodeListView as root view (uses default ViewModel init)
    - Delete Item.swift and ContentView.swift (unused SwiftData template)
    - Xcode project auto-syncs via PBXFileSystemSynchronizedRootGroup

--- ios/Unblur/UnblurApp.swift
+++ ios/Unblur/UnblurApp.swift
@@ -1,32 +1,10 @@
-//
-//  UnblurApp.swift
-//  Unblur
-//
-//  Created by Nathan Zhao on 6/4/2026.
-//
-
 import SwiftUI
-import SwiftData
 
 @main
 struct UnblurApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EpisodeListView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
